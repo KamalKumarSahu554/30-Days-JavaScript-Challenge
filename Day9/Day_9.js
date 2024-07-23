eefb5a6fca7fe29e5f1fd1d73ccbbd8d7d7ca755
“KamalKumarSahu554” <kamalkumar554@outlook.com>
2024-07-24 02:02:13 +0530

    add day9

--- Day9/Day_9.js
+++ Day9/Day_9.js
@@ -1,19 +1,34 @@
 // Activity 1:
 let header = document.getElementById("head");
-header.innerText = "Hello Sir!";
+header.textContent = "DOM Manipulation";
 
-let p1 = document.querySelector(".para");
+let p1 = document.querySelector(".para1");
 p1.style.backgroundColor = "yellow";
 
 // Activity 2:
 let newDiv = document.createElement("div");
-newDiv.innerHTML = "This is a div";
+newDiv.textContent = "This is a new div";
 document.body.appendChild(newDiv);
 
-let ul = document.querySelector("#list");
 let li = document.createElement("li");
-li.innerHTML = "Contact";
-ul.appendChild(li);
+li.innerText = "Contact";
+document.querySelector("#list").append(li);
 
 // Activity 3:
-document.body.removeChild(newDiv);
+let removeDiv = document.getElementsByClassName("remove-div");
+removeDiv[0].remove();
+
+let parent = document.querySelector(".parent");
+let lastChild = parent.lastElementChild;
+lastChild.remove();
+
+// Activity 4:
+let img = document.querySelector("img");
+img.setAttribute(
+  "src",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7yJpEo39N7-g9_RoI3GbRptKdvaAOlvij2Q&s"
+);
+
+let aquaDiv = document.querySelector(".aqua");
+aquaDiv.classList.add("red");
+aquaDiv.classList.remove("blue");
